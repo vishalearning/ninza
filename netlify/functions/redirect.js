1cfe7bc249e25a104b1dde9f27a2719ca3827a61
vishalearning <mrvishalsingh49@gmail.com>
2025-11-14 10:38:58 +0530

    Update redirect.js

--- netlify/functions/redirect.js
+++ netlify/functions/redirect.js
@@ -3,18 +3,6 @@ export async function handler(event, context) {
     const MAX_WHATSAPP = 6;
 
     const encodedLinks = [
-      "aHR0cHM6Ly93YS5tZS85MTgwMDY4NTU5MzE=",
-      "aHR0cHM6Ly93YS5tZS85MTkwNDc0OTk1OTI=",
-      "aHR0cHM6Ly93YS5tZS85MTc5ODMzMjQ3MTQ=",
-      "aHR0cHM6Ly93YS5tZS85MTg5ODQ2OTYxNTE=",
-      "aHR0cHM6Ly93YS5tZS85MTk5ODY3Mjg3NDA=",
-      "aHR0cHM6Ly93YS5tZS85MTkwMjQ3OTg3NzA=",
-      "aHR0cHM6Ly93YS5tZS85MTc1ODQwMTk3NDg=",
-      "aHR0cHM6Ly93YS5tZS85MTk3ODYwMzc1MDU=",
-      "aHR0cHM6Ly93YS5tZS85MTg0MzQzOTYzMDM=",
-      "aHR0cHM6Ly93YS5tZS85MTcwMTE0NzY3OTE=",
-      "aHR0cHM6Ly93YS5tZS85MTcwNjc2OTc1NTQ=",
-      "aHR0cHM6Ly93YS5tZS85MTcwNjEyNzA2MjQ=",
 
       // TELEGRAM LINK
       "aHR0cHM6Ly90Lm1lLyttUjVZdUpvV2tlSXhOemRs",
